56146fc67a8203f2349512b68702599a4a6f8b86
Dipak Nimavat <112933488+dipak1950@users.noreply.github.com>
2022-12-14 13:07:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import About from './Components/About';
+import Testimonial from './Components/Testimonial';
+import Product from './Components/Product';
+import Blog from './Components/Blog';
+import Footer from './Components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Home /> */}
+      <Header />
+      <About />
+      {/* <Testimonial />
+      <Product />
+      <Blog /> */}
+      <Footer />
+
+    </>
   );
 }
 
